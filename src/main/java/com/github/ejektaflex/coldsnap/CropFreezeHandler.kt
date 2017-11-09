4e34b6a674b5c155fb65932897e710fa6201cd8d
Unknown <dootydooter@gmail.com>
2017-11-09 15:57:39 -0600

    Move crop kill method into inner method

--- src/main/java/com/github/ejektaflex/coldsnap/CropFreezeHandler.kt
+++ src/main/java/com/github/ejektaflex/coldsnap/CropFreezeHandler.kt
@@ -1,8 +1,4 @@
-import net.minecraft.block.Block
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
 import net.minecraftforge.event.world.BlockEvent
-import net.minecraftforge.fml.common.eventhandler.Event
 import net.minecraftforge.fml.common.eventhandler.EventPriority
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import toughasnails.api.TANBlocks
@@ -12,34 +8,29 @@ import toughasnails.api.season.SeasonHelper
 import toughasnails.api.config.SeasonsOption
 import java.util.Random
 
-
 class CropFreezeHandler {
     // Random chance gen
     private val random = Random()
     private val config = ColdSnap.config
 
-    fun killCrop(event: BlockEvent.CropGrowEvent.Pre) {
-        val block = event.state.block
-        val cropName = "" + block.registryName!!
-        event.deny()
-
-        if (cropName in config.fragileCrops) {
-            // Cancel growth
-            event.deny()
-            // Change crop to dead crop
-            event.world.setBlockState(event.pos, TANBlocks.dead_crops.defaultState)
-        } else if (cropName in config.partialCrops) {
-            // Cancel growth
-            event.deny()
-            event.world.setBlockState(event.pos, block.defaultState, 3)
-        }
-
-    }
-
     @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onCropGrowPre(event: BlockEvent.CropGrowEvent.Pre) {
+
         val subseason = SeasonHelper.getSeasonData(event.world).subSeason
         val season = subseason.season
+
+         fun killCrop(event: BlockEvent.CropGrowEvent.Pre) {
+            val block = event.state.block
+            val cropName = "" + block.registryName!!
+            event.deny()
+
+            if (cropName in config.fragileCrops) {
+                // Change crop to dead crop
+                event.world.setBlockState(event.pos, TANBlocks.dead_crops.defaultState)
+            } else if (cropName in config.partialCrops) {
+                event.world.setBlockState(event.pos, block.defaultState, 3)
+            }
+        }
 
         // Only grow if the growth check succeeds
         if (random.nextDouble() <= config.baseGrowthChance) {
@@ -53,7 +44,6 @@ class CropFreezeHandler {
                 }
             }
         }
+
     }
-
-
 }
